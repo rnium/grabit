5781477d45bbc5a6488fbee889d34b49dd6fa6c1
rony <rniumo@gmail.com>
2024-07-31 09:37:20 +0600

    multi selector support

--- app/scraping/parsers/list_value_parser.py
+++ app/scraping/parsers/list_value_parser.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
 from ..website_schema import ListValues
 from app.utils.exceptions.selector_exceptions import ListItemSelectorError
-
+from .utils import select_with_raw_selector
 
 def parse_lv(soup: BeautifulSoup, lv_config: ListValues):
-    itm_container = soup.select_one(lv_config.container_selector)
+    itm_container = select_with_raw_selector(soup, lv_config.container_selector)
     if itm_container is None:
         raise ListItemSelectorError(lv_config.container_selector)
-    items_fragment = itm_container.select(lv_config.item_selector)
+    items_fragment = select_with_raw_selector(itm_container, lv_config.item_selector, many=True)
     items = [i[lv_config.attribute] if lv_config.attribute else i.text for i in items_fragment]
     return items
